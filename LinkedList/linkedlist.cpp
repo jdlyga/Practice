b4b7fd2725565fa2f543f15b233d7401cb65b89b
jdlyga <jdlyga@gmail.com>
2015-11-15 22:39:49 -0500

    re-implemented from memory, and added a add to back function

--- LinkedList/linkedlist.cpp
+++ LinkedList/linkedlist.cpp
@@ -9,59 +9,68 @@ using namespace std;
 
 ////////////////////////////////////////////////////////////////////////////////
 
-
-typedef struct linkedList
+template <class TYPE>
+struct linkedList
 {
-  string str;
-  linkedList * next;
-} linkedList;
-
-
-template <class T>
-class listElement
-{
-  public:
-    listElement(const T & value)
-      : data(value)
-    {}
-    ~listElement()
-    {}
-
-    listElement * next = NULL;
-    T data;
+  TYPE data;
+  linkedList * next = NULL;
 };
+////////////////////////////////////////////////////////////////////////////////
 
-bool insertInFront(listElement<string> ** head, string data)
+
+void insertInFront(linkedList<string> *& head, string data)
 {
-  listElement<string> * newElem = new listElement<string>(data);
-  if (not newElem) return false;
+  linkedList<string> * newElem = new linkedList<string>;
   newElem->data = data;
-  newElem->next = *head;
-  *head = newElem;
-  return true;
+  newElem->next = head;
+  head = newElem;
 }
+////////////////////////////////////////////////////////////////////////////////
+
+
+void insertInBack(linkedList<string> *& head, string data)
+{
+  linkedList<string> * newElem = new linkedList<string>;
+  newElem->data = data;
+  newElem->next = NULL;
+
+  linkedList<string> * ptr = head;
+  if (ptr == NULL)
+  {
+    head = newElem;
+  }
+  else
+  {
+    while (ptr->next != NULL)
+    {
+      ptr = ptr->next;
+    }
+    ptr->next = newElem;
+  }
+}
+////////////////////////////////////////////////////////////////////////////////
+
 
 int main()
 {
   printf("Hi!\n");
 
-  listElement<string> * head = NULL;
-  insertInFront(&head, "panda");
-  insertInFront(&head, "salad");
-  insertInFront(&head, "sandwich");
-  insertInFront(&head, "apple");
-  insertInFront(&head, "1");
-  insertInFront(&head, "2");
-  insertInFront(&head, "3");
-  insertInFront(&head, "4");
-  insertInFront(&head, "5");
-  insertInFront(&head, "6");
+  linkedList<string> * head = NULL;
+  insertInBack(head, "panda");
+  insertInBack(head, "salad");
+  insertInBack(head, "sandwich");
+  insertInBack(head, "apple");
+  insertInBack(head, "1");
+  insertInBack(head, "2");
+  insertInBack(head, "3");
+  insertInBack(head, "4");
+  insertInBack(head, "5");
+  insertInBack(head, "6");
 
-  while (head->next != NULL)
+  while (head != NULL)
   {
-    printf("head data: %s\n", head->data.c_str());
+    printf("HERE IT IS: %s\n", head->data.c_str());
     head = head->next;
   }
-
 }
 ////////////////////////////////////////////////////////////////////////////////
