546d35497e3027fbad415fc47ab79a4e743af6d8
jdlyga <jdlyga@gmail.com>
2015-12-20 22:30:44 -0500

    puts a sorted vector of ints into a binary search tree

---
+++ 4/minTree/minTree.cpp
@@ -0,0 +1,73 @@
+#include <cstdio>
+#include <string>
+#include <deque>
+#include <queue>
+#include <algorithm>
+#include <functional>
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+class Node
+{
+  public:
+
+    Node(int d) : data(d) {};
+    ~Node() {};
+
+    int data;
+    Node * left = NULL;
+    Node * right = NULL;
+};
+
+
+void makeBST(vector<int> & v, int left, int right, Node *& root)
+{
+  if (left > right) return;
+  int center = (left + right) / 2;
+  root = new Node(v[center]);
+  makeBST(v, left, center-1, root->left);
+  makeBST(v, center+1, right, root->right);
+}
+
+void printTreeDFS(Node * root, int depth = 0)
+{
+  if (root != NULL)
+  {
+    cout << depth << ") " << root->data << endl;
+    printTreeDFS(root->left, depth+1);
+    printTreeDFS(root->right, depth+1);
+  }
+}
+
+void printTreeBFS(Node * root)
+{
+  queue<Node*> nodequeue;
+  nodequeue.push(root);
+
+  while (!nodequeue.empty())
+  {
+    Node * node = nodequeue.front();
+    nodequeue.pop();
+
+    cout << node->data << endl;
+
+    if (node->left != NULL)
+      nodequeue.push(node->left);
+    if (node->right != NULL)
+      nodequeue.push(node->right);
+  }
+}
+
+
+int main()
+{
+  Node * root = NULL;
+  vector<int> v = {1, 3, 5, 7, 8, 9, 10, 11, 23, 45, 54, 73, 81, 90};
+  makeBST(v, 0, v.size()-1, root);
+
+  printTreeDFS(root);
+
+  return 0;
+}
